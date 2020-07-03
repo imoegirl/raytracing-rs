9d11783f0cf17205c8c5a25742c7c9782bfa1f9c
imoegirl <imoegirl@outlook.com>
2020-07-03 19:43:59 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,33 @@
-use std::io::prelude::*;
-use std::fs::File;
+// use std::io::prelude::*;
+// use std::fs::File;
 
-extern crate math;
-use math::vector::Vector3;
+// extern crate math;
+// use math::vector::Vector3;
 
-extern crate pbr;
-use pbr::ProgressBar;
+// extern crate pbr;
+// use pbr::ProgressBar;
+
+extern crate image;
 
 
 fn main() -> std::io::Result<()>{
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    const IMAGE_WIDTH: u32 = 51200;
+    const IMAGE_HEIGHT: u32 = 51200;
 
-    let mut buffer = File::create("image.ppm")?;
-    let header = format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
-    buffer.write_all(header.as_str().as_bytes())?;
+    // let mut pb = ProgressBar::new((IMAGE_HEIGHT * IMAGE_WIDTH) as u64);
+    // pb.format("╢▌▌░╟");
 
-    let mut pb = ProgressBar::new(IMAGE_HEIGHT as u64);
-    pb.format("╢▌▌░╟");
+    let mut imgbuf = image::ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    let mut j: i32 = IMAGE_HEIGHT - 1;
-    while j >= 0 {
-        let mut i = 0;
-        while i < IMAGE_WIDTH {
-            let r: f64 = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
-            let g: f64 = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
-            let b: f64 = 0.25;
-
-            let color = Vector3::new(r,g,b);
-            write_color(&mut buffer, &color)?;
-
-            i += 1;
-        }
-        pb.inc();
-        j -= 1;
+    for(x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r: u8 = ((x as f64) / ((IMAGE_WIDTH - 1) as f64) * 255.999) as u8;
+        let g: u8 = ((y as f64) / ((IMAGE_HEIGHT - 1) as f64) * 255.999) as u8;
+        let b: u8 = (0.25 * 255.999) as u8;
+        *pixel = image::Rgb([r, g, b]);
+        // pb.inc();
     }
 
-    // pb.finish_print("Done");
+    imgbuf.save("generated.png").unwrap();
   
-    Ok(())
-}
-
-fn write_color(buffer: &mut File, v: &Vector3) -> std::io::Result<()> {
-    buffer.write_all(format!("{} {} {}\n", 
-    v.x * 255.999,
-    v.y * 255.999,
-    v.z * 255.999).as_str().as_bytes())?;
     Ok(())
 }
